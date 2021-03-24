0c1155a62fef156f82eb2e3c19d62bcb3aed9a81
rohan_mittal <rohanmittal1366@gmail.com>
2021-03-24 10:53:46 +0530

    add image in background

--- src/App.js
+++ src/App.js
@@ -20,7 +20,13 @@ class App extends React.Component {
   };
   render() {
     const { advice } = this.state;
-    return <h1>{advice}</h1>;
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{advice}</h1>
+        </div>
+      </div>
+    );
   }
 }
 
