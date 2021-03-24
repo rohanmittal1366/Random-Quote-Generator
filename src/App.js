9ad58a2ea297eac3f6b869f2310a95f20f1385fe
rohan_mittal <rohanmittal1366@gmail.com>
2021-03-24 10:36:06 +0530

    add random quote text

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
+import React from "react";
+import "./App.css";
+import axios from "axios";
+class App extends React.Component {
+  state = { advice: "" };
 
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <h2>Quote generator</h2>
-    </div>
-  );
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+  fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        const { advice } = response.data.slip;
+        this.setState({ advice });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  render() {
+    const { advice } = this.state;
+    return <h1>{advice}</h1>;
+  }
 }
 
 export default App;
